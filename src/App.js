cda816498f50bc20ce17c018f5cd43eb9e40ecd4
Zaid Taiyab <ztaiyab@asu.edu>
2024-06-16 12:57:58 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import ArticleDetails from './pages/ArticleDetails';
 import CreateArticle from './pages/CreateArticle';
+import About from './pages/About';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import NotFound from './pages/NotFound';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/create">Create Article</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+      </ul>
+    </nav>
+  );
+};
 
 const App = () => {
   return (
     <Router>
+      <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/article/:id" component={ArticleDetails} />
         <Route path="/create" component={CreateArticle} />
+        <Route path="/about" component={About} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
