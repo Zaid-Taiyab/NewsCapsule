1bf95b83e16711c4de85a388b5c106c08d3202e0
Zaid Taiyab <ztaiyab@asu.edu>
2024-06-16 12:59:25 -0700

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,1 +1,37 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+  const [form, setForm] = useState({ username: '', email: '', password: '' });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const validate = () => {
+    let formErrors = {};
+    if (!form.username) formErrors.username = 'Username is required';
+    if (!form.email) formErrors.email = 'Email is required';
+    if (!form.password) formErrors.password = 'Password is required';
+    return formErrors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formErrors = validate();
+    if (Object.keys(formErrors).length === 0) {
+      // Dispatch signup action
+    } else {
+      setErrors(formErrors);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" value={form.username} onChange={handleChange} />
+     
 
