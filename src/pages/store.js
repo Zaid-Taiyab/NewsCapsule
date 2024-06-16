070c1059a932d1f2681d5d8db0daec0dfebca1fc
Zaid Taiyab <ztaiyab@asu.edu>
2024-06-16 13:00:35 -0700

    Update store.js

--- src/pages/store.js
+++ src/pages/store.js
@@ -1,1 +1,14 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import authReducer from './reducers/authReducer';
+import articleReducer from './reducers/articleReducer';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  articles: articleReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
 
